6a2ee0b4bc1459632138376bc063e467e76152a5
Shivangi Singh <shivangisingh7@outlook.com>
2026-02-22 16:12:03 +0530

    Create Day22_1.c

---
+++ Day22_1.c
@@ -0,0 +1,43 @@
+/*
+Problem: Count Nodes in Linked List
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the result
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    struct Node* head = NULL;
+    struct Node* temp = NULL;
+    for(int i = 0; i < n; i++) {
+        int val;
+        scanf("%d", &val);
+        struct Node* newNode = createNode(val);
+        if(head ==
