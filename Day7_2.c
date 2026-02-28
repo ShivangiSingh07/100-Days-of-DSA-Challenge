8e14b7970702cdfaeb5c7bae2ddb94238b43c0b2
Shivangi Singh <shivangisingh7@outlook.com>
2026-02-28 20:15:04 +0530

    Update Day7_2.c

--- Day7_2.c
+++ Day7_2.c
@@ -28,8 +28,18 @@ Constraints:
 
 0 <= n <= 30
   */
+#include <stdio.h>
+
+// Recursive Fibonacci
 int fib(int n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
     return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", fib(n));
+    return 0;
 }
