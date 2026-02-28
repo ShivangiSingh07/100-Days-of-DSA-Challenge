bf90a70f589d240afe511f829fddf025d642b5a7
Shivangi Singh <shivangisingh7@outlook.com>
2026-02-28 20:07:29 +0530

    Update Day22_1.c

--- Day22_1.c
+++ Day22_1.c
@@ -31,13 +31,46 @@ struct Node* createNode(int data) {
     return newNode;
 }
 
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
 int main() {
     int n;
     scanf("%d", &n);
+
     struct Node* head = NULL;
-    struct Node* temp = NULL;
-    for(int i = 0; i < n; i++) {
+    struct Node* tail = NULL;
+
+    for (int i = 0; i < n; i++) {
         int val;
         scanf("%d", &val);
         struct Node* newNode = createNode(val);
-        if(head ==
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // Print the linked list elements
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    // Print the count of nodes
+    printf("\nCount = %d\n", countNodes(head));
+
+    return 0;
+}
