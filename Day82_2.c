657dfe72539e45a1b807454ed84dd848665782a6
Shivangi Singh <shivangisingh7@outlook.com>
2026-04-23 20:46:10 +0530

    Create Day82_2.c

---
+++ Day82_2.c
@@ -0,0 +1,57 @@
+/*
+Problem: Given a sorted array of distinct integers and a target value,
+return the index if the target is found. If not, return the index where
+it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+Constraints:
+1 <= nums.length <= 10^4
+-10^4 <= nums[i] <= 10^4
+nums contains distinct values sorted in ascending order.
+-10^4 <= target <= 10^4
+*/
+
+#include <stdio.h>
+
+int searchInsert(int arr[], int n, int target) {
+    int left = 0, right = n - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    // If not found, left will be the insertion position
+    return left;
+}
+
+int main() {
+    int n, target;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &target);
+
+    int result = searchInsert(arr, n, target);
+    printf("%d\n", result);
+    return 0;
+}
