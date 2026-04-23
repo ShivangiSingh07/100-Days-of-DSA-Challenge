b6d2ded5044e0efb0d7b2f4b0dd17f4ef7aca5c6
Shivangi Singh <shivangisingh7@outlook.com>
2026-04-23 20:43:47 +0530

    Create Day81_2.c

---
+++ Day81_2.c
@@ -0,0 +1,48 @@
+/*
+Problem: Given an array of integers nums which is sorted in ascending order,
+and an integer target, write a function to search target in nums.
+If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+
+Constraints:
+1 <= nums.length <= 10^4
+-10^4 < nums[i], target < 10^4
+All the integers in nums are unique.
+nums is sorted in ascending order.
+*/
+
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int target) {
+    int left = 0, right = n - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int n, target;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &target);
+
+    int result = binarySearch(arr, n, target);
+    printf("%d\n", result);
+    return 0;
+}
